22ae7049d316d19b97086bb166a7df013f6e822c
Oleg Guro <ooleg6334@gmail.com>
2019-01-17 23:20:39 +0300

    Add Rakefile, update Gemfile, add modal

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+#подключение к БД
+set :database, "sqlite3:pizzashop.db"
+
+class Product < ActiveRecord::Base
+
+end
 
 get '/' do
 	erb :index		
