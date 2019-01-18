df96fae7e464b32e22f306c9bf24f72b035e238f
Oleg Guro <ooleg6334@gmail.com>
2019-01-18 23:57:30 +0300

    Display products, step 3, add images price & size

--- db/migrate/20190117205840_add_products.rb
+++ db/migrate/20190117205840_add_products.rb
@@ -20,11 +20,41 @@ class AddProducts < ActiveRecord::Migration[5.2]
   	Product.create ({
   	:title => 'Vegetarian', 
   	:description => 'Ammazing Vegetarian pizza',
-  	:price => 400,:size => 30,
+  	:price => 400,
+    :size => 30,
   	:is_spicy => false,
   	:is_veg => true,
   	:is_best_offer => false,
   	:path_to_image => '/images/veg.png'})
 
+    Product.create ({
+    :title => 'Margherita', 
+    :description => 'Traditional Pizzas',
+    :price => 380,
+    :size => 30,
+    :is_spicy => true,
+    :is_veg => true,
+    :is_best_offer => false,
+    :path_to_image => '/images/margherita.png'})
+ 
+    Product.create ({
+    :title => 'Prawn Seafood', 
+    :description => 'Szechuan Chilli Prawn Seafood Pizzas',
+    :price => 400,
+    :size => 30,
+    :is_spicy => true,
+    :is_veg => false,
+    :is_best_offer => false,
+    :path_to_image => '/images/prawn.png'}) 
+
+    Product.create ({
+    :title => 'Vietnamese Chilli Chicken', 
+    :description => 'Vietnamese Chilli Chicken Chicken Pizzas',
+    :price => 410,
+    :size => 30,
+    :is_spicy => true,
+    :is_veg => false,
+    :is_best_offer => false,
+    :path_to_image => '/images/vietnamese.png'}) 
   end
 end
