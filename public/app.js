b1cea2c4a5c04d4b6993b7d05a812264bbc3c711
Oleg Guro <ooleg6334@gmail.com>
2019-01-19 12:50:36 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -16,4 +16,25 @@ function add_to_card(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	//вывод колличества item'ов в корзине
+	alert('Items in your cart: ' + cart_get_number_of_item());
+}
+
+function cart_get_number_of_item()
+{
+	var cnt = 0;
+	
+	for(var i = 0; i < localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); //получаем ключ
+		var value = window.localStorage.getItem(key); //получаем значение, аналог в ruby hh[key] = x
+		if(key.indexOf('product_') ==0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
+
 }
